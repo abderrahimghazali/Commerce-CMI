fa1cdecc55f4541ffa0107aad865801663609c44
Abderrahim GHAZALI <abderahim.ghazali@ext.saint-gobain.com>
2019-02-07 14:19:10 +0100

    Updates

---
+++ src/Plugin/Commerce/PaymentGateway/CmiRedirect.php
@@ -0,0 +1,113 @@
+<?php
+namespace Drupal\commerce_cmi\Plugin\Commerce\PaymentGateway;
+
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Url;
+use Drupal\commerce_payment\Exception\PaymentGatewayException;
+
+/**
+ * Provides the CMI offsite Checkout payment gateway.
+ *
+ * @CommercePaymentGateway(
+ *   id = "cmi_redirect_checkout",
+ *   label = @Translation("CMI (Redirect to CMI)"),
+ *   display_label = @Translation("CMI"),
+ *    forms = {
+ *     "offsite-payment" = "Drupal\commerce_cmi\PluginForm\RedirectCheckoutForm",
+ *   },
+ *   payment_method_types = {"credit_card"},
+ *   credit_card_types = {
+ *     "mastercard", "visa",
+ *   },
+ * )
+ */
+
+class CmiRedirect extends OffsitePaymentGatewayBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+  {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $api_url = !empty($this->configuration['api_url']) ? $this->configuration['api_url'] : '';
+    $api_key = !empty($this->configuration['api_key']) ? $this->configuration['api_key'] : '';
+    $secret_key = !empty($this->configuration['secret_key']) ? $this->configuration['secret_key'] : '';
+
+    $form['api_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API URL'),
+      '#default_value' => $api_url,
+      '#description' => $this->t('API URL from CMI Commerce.'),
+      '#required' => TRUE
+    ];
+    $form['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#default_value' => $api_key,
+      '#description' => $this->t('API Key from CMI Commerce.'),
+      '#required' => TRUE
+    ];
+    $form['secret_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Shared Secret'),
+      '#default_value' => $secret_key,
+      '#description' => $this->t('Shared Secret Key from CMI Commerce subscriptions.'),
+      '#required' => TRUE
+    ];
+    $form['mode']['#access'] = FALSE;
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration()
+  {
+    return [
+        'api_key' => '',
+        'secret_key' => '',
+        'api_url' => '',
+      ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::validateConfigurationForm($form, $form_state);
+    if (!$form_state->getErrors() && $form_state->isSubmitted()) {
+      $values = $form_state->getValue($form['#parents']);
+      $this->configuration['api_key'] = $values['api_key'];
+      $this->configuration['secret_key'] = $values['secret_key'];
+      $this->configuration['api_url'] = $values['api_url'];
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::submitConfigurationForm($form, $form_state);
+    if (!$form_state->getErrors()) {
+      $values = $form_state->getValue($form['#parents']);
+      $this->configuration['api_key'] = $values['api_key'];
+      $this->configuration['secret_key'] = $values['secret_key'];
+      $this->configuration['api_url'] = $values['api_url'];
+    }
+  }
+
+  public function onReturn(OrderInterface $order, Request $request)
+  {
+
+  }
+}
+
+
---
+++ src/PluginForm/CmiRedirect/PaymentCmiForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\commerce_cmi\PluginForm\OffsiteRedirect;
+
+use Drupal\commerce_cmi\Exception\PaymentGatewayException;
+use Drupal\commerce_cmi\PluginForm\PaymentOffsiteForm as BasePaymentOffsiteForm;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+
+class PaymentCmiForm extends BasePaymentOffsiteForm {
+
+}
